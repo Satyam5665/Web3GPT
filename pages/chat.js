9eab565ce678eba978d621bae9ea766f42e77de5
Satyam Singh <96643434+Satyam5665@users.noreply.github.com>
2024-01-03 19:00:09 +0530

    Updated the logic in Chat Page

--- pages/chat.js
+++ pages/chat.js
@@ -1,10 +1,11 @@
 import React from "react";
-
+import { Chatting, Modal } from "../Components/Chat/index";
 
 const chat = () => {
   return (
     <div>
-      
+      <Chatting />
+      <Modal />
       <script type="module" src="script.js"></script>;
     </div>
   );
